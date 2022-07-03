fac16b71ba48cbbfe29b9f10759b8bf0308028c0
Alynn'nari Alish'sundi <alynnnari@gmail.com>
2022-07-03 14:12:45 +0930

    second commit, updating all content

---
+++ highscores.js
@@ -0,0 +1,8 @@
+const highscoresList = document.getElementById('highScoreList');
+const highScores = JSON.parse(localStorage.getiem('highScores')) || [];
+
+highscoresList.innerHTML = highScores
+.map( score => {
+    return (`<li class="high-score">${score.name} - $score.score}</li>`);
+})
+.join("");
